d279bc5765d59cf59d44aadafbeef09423a04ada
orphench <cris_alchemist8@hotmail.com>
2016-04-20 01:53:16 -0300

    Primer parcial Signed-off-by: orphench <cris_alchemist8@hotmail.com>

---
+++ Parcial01/Boligrafo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtroNamespace
+{
+    class Boligrafo
+    {
+        private int _cantidadTinta;
+        private string _color;
+        private string _marca;
+
+        public Boligrafo(string color, string marca)
+        {
+            this._color = color;
+            this._marca = marca;
+            this._cantidadTinta = 0;
+        }
+
+        public Boligrafo(string color, string marca, int cantidadTinta)
+        {
+            this._color = color;
+            this._marca = marca;
+            this._cantidadTinta = cantidadTinta;
+        }
+
+        public Boligrafo(int cantidadTinta, string marca, string color)
+        {
+            this._color = color;
+            this._marca = marca;
+            this._cantidadTinta = cantidadTinta;
+        }
+
+        public Boligrafo(string color, int cantidadTinta, string marca)
+        {
+            this._color = color;
+            this._marca = marca;
+            this._cantidadTinta = cantidadTinta;
+        }
+
+        public void Mostrar()
+        {
+            Console.WriteLine(_color + _marca + _cantidadTinta);
+        }
+    }
+}
---
+++ Parcial01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OtroNamespace.Boligrafo unBoligrafo = new OtroNamespace.Boligrafo(5, "BIC", "NEGRO");
+
+            unBoligrafo.Mostrar();
+        }
+    }
+}
