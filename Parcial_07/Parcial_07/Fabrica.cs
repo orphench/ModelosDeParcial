9b93b24f8496e386af5a4b8d90eedca87696ae2e
orphench <cris_alchemist8@hotmail.com>
2016-05-04 00:06:10 -0300

    Error en la parte final a causa de los elementos eliminados del array, no se puede mostrar Signed-off-by: orphench <cris_alchemist8@hotmail.com>

---
+++ Parcial_07/Parcial_07/Fabrica.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_07
+{
+    class Fabrica
+    {
+        private Operario[] _operarios;
+        private string _razonSocial;
+
+        public Fabrica()
+        {
+            this._operarios = new Operario[5];
+        }
+
+        public Fabrica(string razonSocial):this()
+        {
+            this._razonSocial = razonSocial;
+        }
+
+        private float RetornarCostos()
+        {
+            float costoTotal = 0f;
+
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                costoTotal = costoTotal + this._operarios[i].getSalario();
+            }
+
+            return costoTotal;
+        }
+
+        private int ObtenerIndice()
+        {
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                if (this._operarios.GetValue(i) == null)
+                {
+                    return i;
+                }              
+            }
+
+            return -1;
+        }
+
+        private int ObtenerIndice(Operario ope)
+        {
+            for (int i = 0; i < this._operarios.Length; i++)
+            {
+                if ((object)this._operarios.GetValue(i) == ope)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        private string MostrarOperarios()
+        {
+            string todosLosOperarios = null;
+
+            for (int i = 0; i < this._operarios.Length; i++)
+            {              
+                    todosLosOperarios = todosLosOperarios + this._operarios[i].Mostrar();                  
+
+            }
+
+            return todosLosOperarios;
+        }
+
+        public static void MostrarCosto(Fabrica fbr)
+        {
+            Console.WriteLine("\n{0:C2}",fbr.RetornarCostos());
+        }
+
+        public string Mostrar()
+        {           
+            return "\nRazon Social: " + this._razonSocial + "\n\nOPERARIOS:\n" + MostrarOperarios();
+        }
+
+        public static bool operator ==(Fabrica fbr, Operario op)
+        {
+            bool SeEncuentra = false;
+
+            //for (int i = 0; i < fbr._operarios.Length; i++)
+            //{
+            //    if ((object)fbr._operarios.GetValue(i) == op)
+            //    {
+            //        SeEncuentra = true;
+            //        break;
+            //    }
+            //}
+
+            foreach (Operario item in fbr._operarios)
+            {
+                if ((object)item != null && item == op)
+                {
+                    SeEncuentra = true;
+                    break;
+                }
+            }
+
+            return SeEncuentra;
+        }
+
+        public static bool operator !=(Fabrica fbr, Operario op)
+        {
+            return !(fbr == op);
+        }
+
+        public static Fabrica operator +(Fabrica fbr, Operario op)
+        {
+            if (fbr.ObtenerIndice()==-1)
+            {
+                Console.WriteLine("La Fabrica esta Full");
+            }
+            else
+            {
+                if (fbr == op)
+                {
+                    Console.WriteLine("El Operario ya esta en la fabrica");
+                }
+                else
+                {
+                    fbr._operarios[fbr.ObtenerIndice()] = op;
+                }
+            }
+
+            return fbr;
+        }
+
+        public static Fabrica operator -(Fabrica fbr, Operario op)
+        {
+            //if (fbr.ObtenerIndice() != -1)
+            //{
+                if (fbr == op)
+                {
+                    fbr._operarios[fbr.ObtenerIndice(op)] = null;
+                }
+                else
+                {
+                    Console.WriteLine("No se encontro al empleado!!!");
+                }
+            //}
+            
+            return fbr;     
+        }
+    }
+}
---
+++ Parcial_07/Parcial_07/Operario.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial_07
+{
+    class Operario
+    {
+        private string _apellido;
+        private string _nombre;
+        private int _legajo;
+        private float _salario;
+
+        public Operario()
+        {
+            this._salario = 1500f;
+        }
+
+        public Operario(string nombre, string apellido):this()
+        {
+            this._nombre = nombre;
+            this._apellido = apellido;       
+        }
+
+        public Operario(string nombre, string apellido, int legajo):this(nombre, apellido)
+        {
+            this._legajo = legajo;
+        }
+
+
+        public float getSalario()
+        {
+            return this._salario;
+        }
+
+        public void setAumentarSalario(float aumento)
+        {
+            this._salario = this._salario + (this._salario / (100 / aumento));
+        }
+
+        public string ObtenerNombreYApellido()
+        {
+            return "\nNombre y Apellido: " + this._nombre + ", " + this._apellido;
+        }
+
+        public string Mostrar()
+        {
+            return this.ObtenerNombreYApellido() + "\nLegajo: " + this._legajo + "\nSalario:" + this._salario;
+        }
+
+        public static string Mostrar(Operario ope)
+        {
+            return ope.Mostrar();
+        }
+
+        public static bool operator ==(Operario op1, Operario op2)
+        {
+            bool comparacion = false;
+
+            if (op1.ObtenerNombreYApellido() == op2.ObtenerNombreYApellido())
+            {
+                if (op1._legajo == op2._legajo)
+                {
+                    comparacion = true;
+                }
+            }
+
+            return comparacion;
+        }
+
+        public static bool operator !=(Operario op1, Operario op2)
+        {
+            return !(op1 == op2);
+        }
+    }
+}
